f0d4f0e9d2c21387e95dd6059a1958d1263c1473
kingsark500 <kingsark500@gmail.com>
2025-08-07 00:28:47 +0000

    modified movable file

--- DemoMovable/Program.cs
+++ DemoMovable/Program.cs
@@ -1,2 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace DemoMovable
+{
+    // Define the IMovable interface with Move method
+    public interface IMovable
+    {
+        void Move();
+    }
+
+
+    // Car class implementing IMovable
+    public class Car : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Car is moving");
+        }
+    }
+
+    // Bicycle class implementing IMovable
+    public class Bicycle : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Bicycle is moving");
+        }
+    }
+
+    // Main program to demonstrate the implementation
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Create instances of Car and Bicycle
+            IMovable car = new Car();
+
+            IMovable bicycle = new Bicycle();
+
+
+            // Call Move() method on each instance
+            Console.WriteLine("Testing IMovable implementations:");
+
+            Console.WriteLine("-------------------------------");
+
+            car.Move();
+
+            bicycle.Move();
+
+            Console.WriteLine("\nPress any key to exit...");
+
+            Console.ReadKey();
+        }
+    }
+}
